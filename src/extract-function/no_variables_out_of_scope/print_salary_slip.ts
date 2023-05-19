861d6b838277f03f54f7c2e3b31861abc8433ca0
Fouad Hamdi <1621862+fouadh@users.noreply.github.com@>
2023-05-19 10:34:17 +0200

    add extract function first example

---
+++ src/extract-function/no_variables_out_of_scope/print_salary_slip.ts
@@ -0,0 +1,50 @@
+// Example inspired from Martin Fowler, Refactoring Second Edition, p.109
+// Also Inspired from this kata: https://github.com/sandromancuso/salaryslipkata
+export class Clock {
+  private static now: Date
+
+  static get today() {
+    return Clock.now || new Date()
+  }
+
+  static set today(date: Date) {
+    Clock.now = date
+  }
+}
+
+export type Employee = {
+  id: string
+  name: string
+  annualGrossSalary: number
+  payAt?: Date
+}
+
+export function printSalarySlip(employee: Employee) {
+  printBanner()
+
+  // calculate monthly salary
+  const salary = (employee.annualGrossSalary / 12).toFixed(2)
+
+  // record when the salary will be paid
+  const today = Clock.today
+  employee.payAt = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 10
+  )
+
+  printDetails()
+
+  function printDetails() {
+    console.log(`Employee ID: ${employee.id}`)
+    console.log(`Employee Name: ${employee.name}`)
+    console.log(`Gross Salary: £${salary}`)
+    console.log(`Paid on: ${employee.payAt?.toLocaleDateString()}`)
+  }
+}
+
+function printBanner() {
+  console.log('*********************')
+  console.log('**** Salary Slip ****')
+  console.log('*********************')
+}
