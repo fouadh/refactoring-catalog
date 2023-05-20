512da9433eee05172eb4b33f7bc612f395072b30
Fouad Hamdi <1621862+fouadh@users.noreply.github.com@>
2023-05-20 16:56:56 +0200

    add example 3 of extract function

---
+++ src/extract-function/reassigning a local variable/print_salary_slip.test.ts
@@ -0,0 +1,65 @@
+import { printSalarySlip, Clock, Employee } from './print_salary_slip'
+
+describe('Salary Slip', () => {
+  beforeEach(() => {
+    console.log = jest.fn()
+    Clock.today = new Date('2023-12-12')
+  })
+
+  it('should print header', () => {
+    printSalarySlip({
+      id: 'not relevant',
+      name: 'not relevant',
+      annualGrossSalary: 0,
+    })
+
+    expect(console.log).toHaveBeenCalledWith('*********************')
+    expect(console.log).toHaveBeenCalledWith('**** Salary Slip ****')
+  })
+
+  it('should print employee info', () => {
+    printSalarySlip({
+      id: '123',
+      name: 'Jane Doe',
+      annualGrossSalary: 0,
+    })
+
+    expect(console.log).toHaveBeenCalledWith('Employee ID: 123')
+    expect(console.log).toHaveBeenCalledWith('Employee Name: Jane Doe')
+    expect(console.log).toHaveBeenCalledWith('Paid on: 22/12/2023')
+  })
+
+  it('should record last payment date', () => {
+    const employee: Employee = {
+      id: 'not relevant',
+      name: 'not relevant',
+      annualGrossSalary: 0,
+    }
+
+    printSalarySlip(employee)
+
+    expect(employee.lastPaymentDate?.toLocaleDateString()).toEqual('22/12/2023')
+  })
+
+  describe('monthly gross salary', () => {
+    it('for an annual salary of £5,000.00', () => {
+      printSalarySlip({
+        id: '123',
+        name: 'Jane Doe',
+        annualGrossSalary: 5000,
+      })
+
+      expect(console.log).toHaveBeenCalledWith('Gross Salary: £416.67')
+    })
+
+    it('for an annual salary of £9,060.00', () => {
+      printSalarySlip({
+        id: '123',
+        name: 'Jane Doe',
+        annualGrossSalary: 9060,
+      })
+
+      expect(console.log).toHaveBeenCalledWith('Gross Salary: £745.00')
+    })
+  })
+})
---
+++ src/extract-function/reassigning a local variable/print_salary_slip.ts
@@ -0,0 +1,58 @@
+// Example inspired from Martin Fowler, Refactoring Second Edition, p.112
+// Also Inspired from this kata: https://github.com/sandromancuso/salaryslipkata
+export class Clock {
+  private static now: Date
+
+  static get today() {
+    return Clock.now || new Date()
+  }
+
+  static set today(date: Date) {
+    Clock.now = date
+  }
+}
+
+export type Employee = {
+  id: string
+  name: string
+  annualGrossSalary: number
+  lastPaymentDate?: Date
+}
+
+export function printSalarySlip(employee: Employee) {
+  printBanner()
+  const salary = calculateSalary(employee)
+  recordLastPaymentDate(employee)
+  printDetails(employee, salary.toFixed(2))
+}
+
+function calculateSalary(employee: Employee) {
+  let salary = employee.annualGrossSalary / 12
+  if (employee.annualGrossSalary > 8060) {
+    salary = salary - 0.12 * (employee.annualGrossSalary - 8060) / 12
+  }
+
+  return salary
+}
+
+function recordLastPaymentDate(employee: Employee) {
+  const today = Clock.today
+  employee.lastPaymentDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 10
+  )
+}
+
+function printDetails(employee: Employee, salary: string) {
+  console.log(`Employee ID: ${employee.id}`)
+  console.log(`Employee Name: ${employee.name}`)
+  console.log(`Gross Salary: £${salary}`)
+  console.log(`Paid on: ${employee.lastPaymentDate?.toLocaleDateString()}`)
+}
+
+function printBanner() {
+  console.log('*********************')
+  console.log('**** Salary Slip ****')
+  console.log('*********************')
+}
